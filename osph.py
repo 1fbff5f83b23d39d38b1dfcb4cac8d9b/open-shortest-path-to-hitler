5fef6b336a182a63458bf11cd405f9ae16c234ad
1fbff5f83b23d39d38b1dfcb4cac8d9b <0a02d0db@opayq.com>
2016-08-01 17:48:34 +0200

    fixed not wanting to leave Auschwitz

--- osph.py
+++ osph.py
@@ -5,13 +5,15 @@ q = Queue.Queue(1000)
 def get_links(link, rec_depth, path):
 	try:
 		path.append(link)
-		if  "https://en.wikipedia.org/wiki/Adolf_Hitler" in link or "http://en.wikipedia.org/wiki/Adolf_Hitler": exit(path)
-		if rec_depth >= 2: return
+		if link == "https://en.wikipedia.org/wiki/Adolf_Hitler" or link == "http://en.wikipedia.org/wiki/Adolf_Hitler": exit(path)
+		if rec_depth >= 4: return
 		page = urllib2.urlopen(link).read()
 		urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', page)
 		for link in urls:
 			print link
 			get_links(link, rec_depth+1, path)
-	except: return
+	except Exception as e: 
+		print e
+		return
 		
 get_links("https://en.wikipedia.org/wiki/Auschwitz_concentration_camp", 0, [])
