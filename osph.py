1aaebf9733eced78d3f5a2496405608f44bfd3e0
1fbff5f83b23d39d38b1dfcb4cac8d9b <0a02d0db@opayq.com>
2016-08-01 19:21:07 +0200

    the hitler is now brittler

--- osph.py
+++ osph.py
@@ -1,20 +1,53 @@
 import urllib2, re, Queue, threading
+from urlparse import urljoin
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 
 q = Queue.Queue(1000)
+baseurl = "https://en.wikipedia.org"
+beginning_link = "https://en.wikipedia.org/wiki/Auschwitz_concentration_camp"
 
-def get_links(link, rec_depth, path):
+def pretty_print(path):
+	print beginning_link.lower()
+	for tuple in path:
+		print tuple[1]
+	exit()
+
+def get_path(origin, link, path_the_second, path):
+	if origin == link:
+		path_the_second.pop()
+		pretty_print(list(reversed(path_the_second)))
+	for tuple in path:
+		if tuple[1] == origin:
+			path_the_second.append(tuple)
+			get_path(tuple[0], tuple[1], path_the_second, path)
+			return
+
+def get_links(link, rec_depth, origin, path):
 	try:
-		#path.append(link)
-		if "hitler" in link.lower(): print link
-		if link.lower() == "https://en.wikipedia.org/wiki/adolf_hitler" or link.lower() == "http://en.wikipedia.org/wiki/adolf_hitler": exit(path)
+		path.append((origin, link))
+		origin = link
 		if rec_depth >= 2: return
+		link = link.lower()
 		page = urllib2.urlopen(link).read()
 		for link in BeautifulSoup(page, parseOnlyThese=SoupStrainer('a')):
 			if link.has_key('href'):
-				get_links("https://en.wikipedia.org"+link['href'], rec_depth+1, path)
+				link = link['href']
+				link = link.lower()
+				if link.startswith("#"): continue
+				if "file:" in link or "help:" in link: continue
+				if not link.startswith("http"):
+					link = urljoin(baseurl, link)
+				
+				if link == "https://en.wikipedia.org/wiki/adolf_hitler" or link == "http://en.wikipedia.org/wiki/adolf_hitler":
+					path.append((origin, link))
+					for tuple in path:
+						if tuple[1] == "https://en.wikipedia.org/wiki/adolf_hitler" or tuple[1] == "http://en.wikipedia.org/wiki/adolf_hitler":
+							path_the_second = [tuple]
+							get_path(origin, link, path_the_second, path)
+					exit()
+				get_links(link, rec_depth+1, origin, path)
 	except Exception as e:
 		print e
 		return
 		
-get_links("https://en.wikipedia.org/wiki/Auschwitz_concentration_camp", 0, [])
+get_links(beginning_link, 0, beginning_link, [])
